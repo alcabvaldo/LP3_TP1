541ad7e256947d5f5c0a3a963aa6b95ce148f689
alcabvaldo <jandronico@fpuna.edu.py>
2021-08-20 23:23:14 -0400

    arregla capitulo 1

--- TP-LISTINGS/src/capitulo_1/1.1/main.c
+++ TP-LISTINGS/src/capitulo_1/1.1/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include "reciprocal.hpp"
 #include <stdlib.h>
+#include <string.h>
 
 int main (int argc, char **argv)
 {
-    int i;i = atoi (argv[1]);
+    int i;
+    if (argc>1){
+        i = atoi (argv[1]);
+    }else{
+        i=2;
+    }
     printf ("The reciprocal of %d is %g\n", i, reciprocal (i));
     return 0;
+
 }
