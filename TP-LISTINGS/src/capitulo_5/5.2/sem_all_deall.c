3a3a0db4e485d68b9e14bc41c7e71870a54cbf0b
aranusco <araselly2000@gmail.com>
2021-09-12 01:00:46 -0400

    agrega listing 5.2

---
+++ TP-LISTINGS/src/capitulo_5/5.2/sem_all_deall.c
@@ -0,0 +1,44 @@
+#include <sys/ipc.h>  
+#include <sys/sem.h>  
+#include <sys/types.h>  
+#define KEY 1000
+/* We must define union semun ourselves. */  
+ 
+union semun { 
+    int val;  
+    struct semid_ds *buf;  
+    unsigned short int *array;  
+    struct seminfo *__buf;  
+};  
+ 
+/* Obtain a binary semaphore's ID, allocating if necessary. */  
+ 
+int binary_semaphore_allocation (key_t key, int sem_flags)  
+{ 
+    return semget (key, 1, sem_flags);  
+}  
+ 
+/* Deallocate a binary semaphore. All users must have finished their  
+   use. Returns -1 on failure. */  
+ 
+int binary_semaphore_deallocate (int semid)  
+
+{ 
+     union semun ignored_argument;  
+     return semctl (semid, 1, IPC_RMID, ignored_argument);  
+} 
+int main(){
+    //alocar
+    int nroSem = binary_semaphore_allocation(KEY, IPC_CREAT); 
+    int deall;
+    if (nroSem != -1 ){
+        printf("Allocation posible, sem Nro %d", nroSem);
+        deall = binary_semaphore_deallocate(nroSem);
+        if(deall == -1 )
+            printf("No sepudo desallocar,sem nro %d", nroSem);
+        else
+            printf("Desallocado con exitod, sem nro %d", nroSem);
+    }
+    else 
+        printf("Allocation imposible");
+}
