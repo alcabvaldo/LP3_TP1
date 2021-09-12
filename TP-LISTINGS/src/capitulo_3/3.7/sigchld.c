4330231bb9cc85161d7d70285119c0a95f9a579c
alcabvaldo <jandronico@fpuna.edu.py>
2021-09-11 21:17:18 -0400

    added 3.7 no funca

---
+++ TP-LISTINGS/src/capitulo_3/3.7/sigchld.c
@@ -0,0 +1,50 @@
+#include <signal.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/wait.h> 
+#include <unistd.h> //para getpid
+#include <stdio.h>
+
+
+sig_atomic_t child_exit_status; 
+
+
+
+void clean_up_child_process (int signal_number) 
+{ 
+    printf("yo entre aca wtf");
+    /* Clean up the child process. */ 
+    int status; 
+    wait (&status); 
+    /* Store its exit status in a global variable. */ 
+    child_exit_status = status; 
+} 
+
+
+int main () 
+{ 
+    /* Handle SIGCHLD by calling clean_up_child_process. */ 
+    struct sigaction sigchld_action; 
+    memset (&sigchld_action, 0, sizeof (sigchld_action)); 
+    sigchld_action.sa_handler = &clean_up_child_process; 
+    sigaction (SIGCHLD, &sigchld_action, NULL); 
+
+    /* Now do things, including forking a child process. */ 
+    /* ... */     
+    pid_t child_pid; 
+    
+    printf ("the main program process ID is %d\n", (int) getpid());
+
+    child_pid = fork () ; 
+    if (child_pid != 0) {
+        printf ("this is the parent process, with id %d\n", (int) getpid ()); 
+        printf ("the child's process ID is %d\n",(int) child_pid ); 
+    } 
+    else{
+        //while(1) 
+            printf ("this is the child process, with id %d\n", (int) getpid ()); 
+    }
+    //clean_up_child_process
+
+    return 0; 
+} 
