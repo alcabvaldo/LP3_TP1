30242e43ca353bed27bcc3e31ea05ff688543a1f
alcabvaldo <jandronico@fpuna.edu.py>
2021-09-11 22:16:47 -0400

    arregla 4.4, problema era g++

--- TP-LISTINGS/src/capitulo_4/4.4/primes.c
+++ TP-LISTINGS/src/capitulo_4/4.4/primes.c
@@ -1,7 +1,5 @@
 #include <pthread.h> 
 #include <stdio.h> 
-
-
 /* Compute successive prime numbers (very inefficiently). Return the 
  Nth prime number, where N is the value pointed to by *ARG. */ 
 void* compute_prime (void* arg) 
@@ -15,15 +13,16 @@ void* compute_prime (void* arg)
         for (factor = 2; factor < candidate; ++factor) 
             if (candidate % factor == 0) { 
                 is_prime = 0; 
-            break; 
+                break; 
         } 
         /* Is this the prime number we're looking for? */ 
         if (is_prime) { 
             if (--n == 0) 
-                /* Return the desired prime number as the thread return value. */ 
+                /* Return the desired prime number as the thread return 
+                value. */ 
                 return (void*) candidate; 
         } 
-        ++candidate; 
+            ++candidate; 
     } 
     return NULL; 
 } 
@@ -34,17 +33,13 @@ int main ()
     pthread_t thread; 
     int which_prime = 5000; 
     int prime; 
-
     /* Start the computing thread, up to the 5,000th prime number. */ 
-    pthread_create (&thread, NULL, &compute_prime, &which_prime);
-
+    pthread_create (&thread, NULL, &compute_prime, &which_prime); 
     /* Do some other work here... */ 
-    printf("soy el proceso principal y estoy esperando que el hilo termine e calcularse\n");
-    /* Wait for the prime number thread to complete, and get the result. */ 
+    /* Wait for the prime number thread to complete, and get the 
+    result. */ 
     pthread_join (thread, (void*) &prime); 
-
     /* Print the largest prime it computed. */ 
     printf("The %dth prime number is %d.\n", which_prime, prime); 
-
     return 0; 
 } 
