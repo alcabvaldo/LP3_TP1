aea9b1b0b7a808341763622058c1af190ddef534
alcabvaldo <jandronico@fpuna.edu.py>
2021-09-12 23:39:44 -0400

    arregla 4.12

--- TP-LISTINGS/src/capitulo_4/4.12/job-queue3.c
+++ TP-LISTINGS/src/capitulo_4/4.12/job-queue3.c
@@ -2,11 +2,24 @@
 #include <pthread.h> 
 #include <semaphore.h> 
 
+int contador_global=0;
+
+
+
 struct job { 
- /* Link field for linked list. */ 
- struct job* next; 
- /* Other fields describing work to be done... */ 
+    /* Link field for linked list. */ 
+    struct job* next; 
+    /* Other fields describing work to be done... */
+    int num;
+    int cuadrado;
 }; 
+
+void process_job(struct job* job) {
+    job->cuadrado = job->num*job->num;
+    
+    printf("el cuadrado de %d es: %d\n", job->num, job->cuadrado);
+}
+
 
 /* A linked list of pending jobs. */ 
 struct job* job_queue; 
@@ -61,7 +74,8 @@ void enqueue_job (/* Pass job-specific data here... */)
     struct job* new_job; 
     /* Allocate a new job object. */ 
     new_job = (struct job*) malloc (sizeof (struct job)); 
-    /* Set the other fields of the job struct here... */ 
+    /* Set the other fields of the job struct here... */
+    new_job->num = contador_global++; 
 
     /* Lock the mutex on the job queue before accessing it. */ 
     pthread_mutex_lock (&job_queue_mutex); 
@@ -75,4 +89,32 @@ void enqueue_job (/* Pass job-specific data here... */)
     
     /* Unlock the job queue mutex. */ 
     pthread_mutex_unlock (&job_queue_mutex); 
+}
+
+
+
+
+int main(){
+    int num_de_threads = 5;
+    pthread_t thread_id[num_de_threads];
+    printf("Se crean %d hilos\n",num_de_threads);
+
+    for (int i = 0; i < num_de_threads; i++) {
+        //crea el hilo
+        pthread_create(&thread_id[i], NULL, &thread_function, NULL);
+    }
+    
+    //crea los trabajos
+    for (int i = 0; i < 20; i++) {
+        enqueue_job();
+    }
+
+
+
+    // espera a que todos los thread terminen
+    for (int i=0;i<num_de_threads;i++){
+        pthread_join(thread_id[i],NULL);
+    }
+    
+	return 0;
 }
