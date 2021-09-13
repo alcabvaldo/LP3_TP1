e6fec52c5d9a50211fe08843061c2a7b76fa7f5a
alcabvaldo <jandronico@fpuna.edu.py>
2021-09-12 21:30:42 -0400

    arregla 4.8

--- TP-LISTINGS/src/capitulo_4/4.8/cleanup.c
+++ TP-LISTINGS/src/capitulo_4/4.8/cleanup.c
@@ -1,5 +1,7 @@
 #include <malloc.h> 
 #include <pthread.h> 
+#include <time.h> //para el random
+#include <stdlib.h>
 
 /* Allocate a temporary buffer. */ 
 void* allocate_buffer (size_t size) 
@@ -10,6 +12,7 @@ void* allocate_buffer (size_t size)
 /* Deallocate a temporary buffer. */ 
 void deallocate_buffer (void* buffer) 
 { 
+    printf("se dealoco un buffer\n");
     free (buffer); 
 } 
 
@@ -24,9 +27,38 @@ void do_some_work ()
 
     /* Do some work here that might call pthread_exit or might be 
     cancelled... */ 
+    if (rand()%2==1){
+        printf("un hilo llamó a exit\n");
+        pthread_exit(0);
+    }
     
     /* Unregister the cleanup handler. Because we pass a nonzero value, 
     this actually performs the cleanup by calling 
     deallocate_buffer. */ 
     pthread_cleanup_pop (1); 
+}
+
+
+
+int main(void) {
+    srand(time(NULL)); //seed nueva
+    
+    //tenemos tantos thread como elementos en producto[][]
+    int num_de_threads = 5;
+    pthread_t thread_id[num_de_threads];
+    printf("Se crean %d hilos\n",num_de_threads);
+
+    for (int i = 0; i < num_de_threads; i++) {
+        //crea el hilo
+        pthread_create(&thread_id[i],NULL,&do_some_work,NULL);
+    }
+    
+    // espera a que todos los thread terminen
+    for (int i=0;i<num_de_threads;i++){
+        pthread_join(thread_id[i],NULL);
+    }
+
+    printf("como vemos, todos los hilos se desalocaron\n");
+
+    return 0;
 }
